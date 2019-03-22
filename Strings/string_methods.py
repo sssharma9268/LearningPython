06982670807ff69f932223405d20cbee6db8fe39
Sumit <sssharma9278@gmail.com>
2019-03-22 16:45:33 +0530

    Pushing a string_methods program from laptop

---
+++ Strings/string_methods.py
@@ -0,0 +1,30 @@
+name = "john doe"
+name = name.upper()
+print(name)
+
+name = name.lower()
+print(name)
+name = name.title()
+print(name)
+print(name.islower())
+print(name.isupper())
+uname = "john2"
+print(uname.isalpha())
+print(uname.isalnum())
+print(uname.isnumeric())
+print(uname.index('h'))
+print(uname.find('n'))  #find and index method works the same, returns the 1st occurence of letter or word
+msg = "Hi, sumit sharma"
+print(msg.replace('sumit','shantanu'))
+print(msg)  #the strings are immutable
+print(msg.split()) #if no splitting criteria is specified then considers space by default
+
+
+
+
+
+
+
+
+
+
